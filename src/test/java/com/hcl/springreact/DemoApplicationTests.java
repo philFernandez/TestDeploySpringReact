d04546884797c188b70faebbef98328916b5d590
Phil Fernandez <norcal618@gmail.com>
2021-02-07 01:22:51 -0800

    first commit

---
+++ src/test/java/com/hcl/springreact/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.springreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
